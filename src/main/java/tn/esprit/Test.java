d8401b387353877d90ecc4e322122a4c707fc8c2
Theo Wakassia Pago <theopago1@gmail.com>
2021-12-29 17:27:29 +0100

    Commentaires

--- src/main/java/tn/esprit/Test.java
+++ src/main/java/tn/esprit/Test.java
@@ -1,6 +1,10 @@
 package tn.esprit;
+/**
+ Comments
+ */
 public class Test{
     public static void main(String[] args) {
+      //---------------------------
       System.out.println("Bonjour Bangui!"); 
     }  
 }
